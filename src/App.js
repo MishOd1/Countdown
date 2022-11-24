89a9bd401f33a882c6462591172c9e008f8cd1c1
MishOd1 <104631140+MishOd1@users.noreply.github.com>
2022-11-24 22:11:35 +0300

    1 file modified, 2 added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './style.css'
 import React from 'react'
+import EDU from './Components/countDownTimer/Utils/HalvingEDU.js'
 
 import CountdownTimer from './Components/countDownTimer/countDownTimer';
 
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
      <CountdownTimer
      countdownTimestampMs={1711251267000}/>
+      <EDU />
     </div>
   );
 }
---
+++ src/Components/countDownTimer/Utils/HalvingEDU.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import './HalvingEDU.css'
+
+
+function EDU(){
+
+return(
+  <div>
+
+    <p className="para">
+      from a more techinical point of view,After every 210,000 blocks mined, or roughly every four years, the block reward given to Bitcoin miners<br></br>
+       for processing transactions is cut in half. This event is referred to as halving because it cuts in half the rate at which new bitcoins are released <br></br>
+       into circulation. This is Bitcoin's way of enforcing synthetic price inflation until all bitcoins are released.
+
+      This rewards system will continue until around the year 2140, when the proposed limit of 21 million coins is reached. At that point, miners will be rewarded with fees, which network users will pay, for processing transactions. These fees ensure that miners still have the incentive to mine and keep the network going.
+      1
+
+      The halving event is significant because it marks another drop in the rate of new Bitcoins being produced as it approaches its finite supply: the maximum total supply of bitcoins is 21 million. As of late August 2022, there are about 19.1 million bitcoins already in circulation, leaving just around 1.9 million left to be released via mining rewards.
+      4
+
+      In 2009, the reward for each block in the chain mined was 50 bitcoins. After the first halving, it was 25, and then 12.5, and then it became 6.25 bitcoins per block as of May 11, 2020. To put this in another context, imagine if the amount of gold mined out of the Earth was cut in half every four years. If gold's value is based on its scarcity, then a "halving" of gold output every four years would theoretically drive its price higher.
+    </p>
+
+  </div>
+
+
+)
+
+
+}
+
+
+export default EDU
