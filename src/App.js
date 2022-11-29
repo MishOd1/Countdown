c4454b162bf378b8dfd9bc5d659d224e8995e3ad
MishOd1 <104631140+MishOd1@users.noreply.github.com>
2022-11-29 11:43:49 +0300

    2 files modified, 1 added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import EDU from './Components/countDownTimer/Utils/HalvingEDU.js'
 import Fotter2 from './Fotter.js'
 import CountdownTimer from './Components/countDownTimer/countDownTimer';
 import Graph from './Graph.js'
+import Graph2 from './Graph2.js'
 function App() {
   return (
     <div>
@@ -11,8 +12,14 @@ function App() {
       <div className="App">
       <CountdownTimer
       countdownTimestampMs={1711251267000}/>
+        <div className="Graph2">
+        <Graph2/>
+        </div>
         <EDU />
-        <Graph />
+        <div className="Graph1">
+          <Graph />
+        </div>
+        
       </div>
       <footer className="footerMain">
           <Fotter2 />
